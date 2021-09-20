ee595820dd8e606a76d50d275cff92563bdbea59
diningX <diningx.inc@gmail.com>
2021-09-20 13:49:37 +0000

    commitment

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if not firebase_admin._apps:
     #print(json_file)
     #print('=========================')
 
-    cred = credentials.Certificate(json.loads(os.environ['FIREBASE_CONFIG']))
+    cred = credentials.Certificate(keys)
     firebase_admin.initialize_app(cred)
 
 st.session_state['db'] = firestore.client()
